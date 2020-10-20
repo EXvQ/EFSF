b93b84f75391e15cdf50c708235dabd0fec3d426
EXvQ <zm19248@josai.ac.jp>
2020-10-20 06:22:07 +0000

    21

--- ex06-5.js
+++ ex06-5.js
@@ -5,5 +5,5 @@
 //function taro() {who = '太郎';}
 //function hanako() {who = '花子';}
 var who = 'world';function sayhello() {alert('Hello, ' + who + '!');}
-function taro()  {who ='太郎';alert(who+'が来た');}
-function hanako()  {who ='花子';alert(who+'が来た');}
+function taro()  {who ='太郎';alert(who +'が来た');}
+function hanako()  {who ='花子';alert(who +'が来た');}
