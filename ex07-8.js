a7774635f41c796b0e6bb4d6f19ee9ca6f7dc75a
EXvQ <zm19248@josai.ac.jp>
2021-01-26 02:55:50 +0000

    TR-35

---
+++ ex07-8.js
@@ -0,0 +1,3 @@
+//function nazo() {if (document.getElementById('answer').value = '夢') {alert('あたり！');} //
+//{if (document.getElementById('answer').value = "else") {alert('はずれ！');} //
+function nazo() {if (document.getElementById('answer').value = '夢') {alert('あたり！');} else {alert('はずれ！');}}
