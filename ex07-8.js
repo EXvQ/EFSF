0be47f03882e37ab82163cc770a6f0b7a7023de9
EXvQ <zm19248@josai.ac.jp>
2021-02-01 05:24:47 +0000

    TR-37

--- ex07-8.js
+++ ex07-8.js
@@ -1,4 +1,5 @@
 //function nazo() {if (document.getElementById('answer').value = '夢') {alert('あたり！');} //
 //{if (document.getElementById('answer').value = "else") {alert('はずれ！');} //
-function nazo78() {if (document.getElementById('answer').value = '夢') {alert('あたり！');} else {alert('はずれ！');}}
+function nazo78() {
+    if (document.getElementById('answer').value == '夢') {alert('あたり！');} else {alert('はずれ！');}}
 //if (document.getElementById('answer').value =  else {alert('はずれ！');}}//
